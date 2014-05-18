e6c2fdd3fca83be17c21d28f4ea11016f3036ed3
nepta <nepta23@gmail.com>
2014-05-19 01:31:43 +0200

    ignore class without inheritance

--- cpp2uml.lua
+++ cpp2uml.lua
@@ -4,14 +4,18 @@ for index,headerFile in ipairs(arg) do
 	if headerFile:match(".h$") then
 		local file = io.open(headerFile):read("*a")
 
-		local classPattern = "class[%s]+([%w%s:_,]-)[%s]+{"
+		local classPattern = "class[%s]+([%w%s:_,]-)[%s]*{"
 		local classHead = file:match(classPattern)
 
 		local baseClass, motherClass = classHead:match("(%w+)%s+:%s+([%s,%w]+)")
-		motherClass = motherClass:gmatch("(public%s+%w+)")
+		if motherClass then
+			motherClass = motherClass:gmatch("(public%s+%w+)")
 
-		for mother in motherClass do
-			print("\t"..mother:gsub("public%s+","").." <|-- "..baseClass)
+			for mother in motherClass do
+				print("\t"..mother:gsub("public%s+","").." <|-- "..baseClass)
+			end
+		else
+			print("\t"..(baseClass or ""))
 		end
 	end
 end
