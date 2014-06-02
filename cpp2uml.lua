5a9746f3f50085a0188e20afabd73fa3e7ac8f64
nepta <nepta23@gmail.com>
2014-06-02 07:05:51 +0200

    store class member

--- cpp2uml.lua
+++ cpp2uml.lua
@@ -1,15 +1,16 @@
 #!/usr/bin/lua
 print("@startuml")
-local method = {}
-local attribute = {}
+
 for index,headerFile in ipairs(arg) do
 	if headerFile:match(".h$") then
 		local file_ = io.open(headerFile)
 		local file = file_:read("*a")
 		file_:close()
 		file = file:gsub("::","_"):gsub("#endif",":")
-		local classIt = file:gmatch("(.-):")
+		local method = {}
+		local attribute = {}
 
+		local classIt = file:gmatch("(.-):")
 		local classPattern = "class[%s]+([%w%s:_,]-)[%s]*{"
 		local classHead = file:match(classPattern)
 
@@ -28,22 +29,22 @@ for index,headerFile in ipairs(arg) do
 			chunk = chunk:gsub("//.-\n%s",""):gsub("/\*.*\*/","")
 			for member in chunk:gmatch("%s(.-);%s") do
 				if member:match("%(") then
-					print("method: ", member)
---					table.insert(method,member)
+--					print("method: ", member)
+					table.insert(method,member)
 				else
-					print("attribute: ",member)
---					table.insert(attribute,member)
+--					print("attribute: ",member)
+					table.insert(attribute,member)
 				end
 			end
 		end
+		print("method:")
+		for k,v in pairs(method) do
+			print(v)
+		end
+		print("\nattribute: ")
+		for k,v in pairs(attribute) do
+			print(v)
+		end
 	end
 end
---print("method:")
---for k,v in pairs(method) do
---	print(v)
---end
---print("\nattribute: ")
---for k,v in pairs(attribute) do
---	print(v)
---end
 print("@enduml")
